b58f6cdbbf800580e50990862430fe49c2e134f4
tasidora08 <tasi.dora08@gmail.com>
2026-01-23 09:15:09 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+if (navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}    
+
+if (navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+} 
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const Linkaction = ()=>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click', Linkaction))
+
+/*=============== HOME TYPED JS ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== CONTACT EMAIL JS ===============*/ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
