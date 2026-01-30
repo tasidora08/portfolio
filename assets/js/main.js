80c0391ede57a6da3244feb9e662a8b35197e299
tasidora08 <tasi.dora08@gmail.com>
2026-01-30 11:01:51 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,23 @@ const Linkaction = ()=>{
 navLink.forEach(n=> n.addEventListener('click', Linkaction))
 
 /*=============== HOME TYPED JS ===============*/
-
+var typed = new Typed('#home-subtitle', {
+      strings: ['Developer', 'Webdesigner', 'Fronted manager', 'Freelancer'],
+      typeSpeed: 50,
+      loop:true,
+      backSpeed:40,
+      backDelay:2000,
+      cursor:'_'
+    });
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader=()=>{
+    const header = document.getElementById('header')
+    this.scrollY>50 ? header.classList.add('shadow-header'):
+    header.classList.remove('shadow-header')
+}
 
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== CONTACT EMAIL JS ===============*/ 
 
